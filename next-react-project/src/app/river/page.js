b149c9867b012eddb84304c6427ee2176b37f745
ceoDemitri <demichole@gmail.com>
2025-10-27 00:36:02 -0500

    feat: new river page using AppRouter. muah. adds nav component to layout showcases power of layout page

---
+++ next-react-project/src/app/river/page.js
@@ -0,0 +1,13 @@
+export default function River() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-blue-600">
+        The{" "}
+        <span className="underline italic uppercase text-blue-300 text-shadow-lg shadow-red-500/50">
+          River
+        </span>{" "}
+        Page
+      </h1>
+    </main>
+  );
+}
